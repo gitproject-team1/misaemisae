dd52a131adde747e037ac50b1774191af6e6c83d
JYROH <ghgt1@naver.com>
2023-01-14 00:55:36 +0900

    feat: Create Bottom Nav component

--- src/components/UI/Container.jsx
+++ src/components/UI/Container.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from "./Container.module.scss";
-import styled from "styled-components";
 
-// props= {children, width, a, b,c }
 const Container = ({ children, width }) => {
   return (
     <section className={styles.container} style={{ width: width }}>
@@ -10,13 +8,4 @@ const Container = ({ children, width }) => {
     </section>
   );
 };
-
-// const Container = styled.section`
-//   background-color: #ffffff;
-//   border-radius: 8px;
-//   margin: 1rem, 0.75rem;
-//   text-align: center;
-//   width: ${(props) => props.width};
-// `;
-
 export default Container;
