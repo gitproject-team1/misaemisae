3defed4506a54ea3359c924e69e6d75d42227b36
JYROH <ghgt1@naver.com>
2023-01-13 13:48:26 +0900

    feat: Add Sass variable and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import Welcome from "./pages/Welcome";
-import "./App.css";
+import Welcome from "./pages/Welcome/Welcome";
+import "./App.scss";
 
 function App() {
   return (
