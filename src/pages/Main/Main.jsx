d0a4eb3b7cbe32b505042d06569b41ab4ba1cc63
1myeji <106291546+1myeji@users.noreply.github.com>
2023-01-13 20:33:49 +0900

    feat: Create StanDard Time content

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -8,16 +8,21 @@ const Main = () => {
   }, []);
 
   const [defaultPlace, setDefaultPlace] = useState(JSON.parse(localStorage.getItem("defaultPlace")));
-  const sido = defaultPlace[0];
-  console.log(sido);
-  const fetchData = async (sido) => {
-    const response = await getInfo(sido);
+  const [sido, setSido] = useState(defaultPlace[0]);
+  const [time, setTime] = useState("");
+
+  const fetchData = async () => {
+    const res = await getInfo(sido);
+    const time = res.response.body.items.find((data) => {
+      return data.sidoName === sido;
+    });
+    setTime(time.dataTime);
   };
 
   return (
     <div className={styles.container}>
       <h2 className={styles.header}>{defaultPlace.join(" ")} 미세먼지 농도는 다음과 같습니다</h2>
-      <h3></h3>
+      <h3 className={styles.time}>{time} 기준</h3>
     </div>
   );
 };
