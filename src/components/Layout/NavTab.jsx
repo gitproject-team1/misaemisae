97322a03233007c5b6d19d14ccea4029297f23f4
JYROH <ghgt1@naver.com>
2023-01-16 14:56:05 +0900

    feat: Add Nav Main Tab and animations

--- src/components/Layout/NavTab.jsx
+++ src/components/Layout/NavTab.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./NavTab.module.scss";
 
-const NavTab = ({ children, content }) => {
+const NavTab = ({ children, content, setNavStatus }) => {
   const navigate = useNavigate();
 
   let link = "";
   const handleNavClick = () => {
+    setNavStatus(false);
     if (content === "정보변경") {
       localStorage.removeItem("defaultPlace");
       navigate("/");
+    } else if (content === "즐겨찾기") {
+      navigate("/");
+    } else if (content === "먼지순위") {
+      navigate("/rank");
+    } else if (content === "메인페이지") {
+      navigate("/main");
     }
   };
 
