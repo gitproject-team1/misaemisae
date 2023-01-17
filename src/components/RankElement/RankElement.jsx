4b99768d84f696336b3cf066e26859e550c57990
JYROH <ghgt1@naver.com>
2023-01-17 21:01:23 +0900

    feat: Create Rank Components

--- src/components/RankElement/RankElement.jsx
+++ src/components/RankElement/RankElement.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import styles from "./RankElement.module.scss";
 
-const RankElement = () => {
-  return <div>RankElement</div>;
+const RankElement = ({ sido, pm10, pm25, index }) => {
+  let pmGrade = "";
+  let color = "#666666";
+  if (pm10 != "-") {
+    if (pm10 <= 30) {
+      color = "#1e64ee";
+      pmGrade = "좋음";
+    } else if (pm10 <= 80) {
+      color = "#00d500";
+      pmGrade = "보통";
+    } else if (pm10 <= 150) {
+      color = "#f95a20";
+      pmGrade = "나쁨";
+    } else {
+      color = "#e73532";
+      pmGrade = "매우 나쁨";
+    }
+  } else pmGrade = "측정중";
+
+  return (
+    <div className={styles.container}>
+      <span>{index + 1}</span>
+      <span>{sido}</span>
+      <span style={{ color: color }}>{pmGrade}</span>
+      <div className={styles.pm}>
+        <div>
+          <span>미세먼지</span>
+          <span>{pm10}</span>
+        </div>
+        <div>
+          <span>초미세먼지</span>
+          <span>{pm25}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RankElement;
--- src/pages/Rank/Rank.jsx
+++ src/pages/Rank/Rank.jsx
@@ -1,12 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../../components/UI/Container";
 import styles from "./Rank.module.scss";
+import getInfo from "../../api/requests";
+import RankElement from "../../components/RankElement/RankElement";
 
 const Rank = () => {
+  const [average10, setAverage10] = useState([]);
+  const [average25, setAverage25] = useState([]);
+
+  let sidoName = [
+    "서울",
+    "부산",
+    "대구",
+    "인천",
+    "광주",
+    "대전",
+    "울산",
+    "경기",
+    "강원",
+    "충북",
+    "충남",
+    "전북",
+    "전남",
+    "경북",
+    "경남",
+    "제주",
+    "세종",
+  ];
+
+  const getSidoInfo = async () => {
+    const promises = [];
+    sidoName.forEach((sido) => {
+      const res = getInfo(sido);
+      promises.push(res);
+    });
+    // promse.all로 한번에 받아오기
+    let promiseRes = await Promise.all(promises);
+    promiseRes.forEach((sido, i) => {
+      let stationCount = 0;
+      let pm10Sum = 0;
+      let pm25Sum = 0;
+      sido.response.body.items.forEach((station) => {
+        if (station.pm10Value !== "-") {
+          stationCount++;
+          pm10Sum += Number(station.pm10Value);
+        }
+        if (station.pm25Value !== "-") {
+          pm25Sum += Number(station.pm25Value);
+        }
+      });
+      setAverage10((prevState) => [
+        ...prevState,
+        [i, sidoName[i], parseInt(pm10Sum / stationCount)],
+      ]);
+      setAverage25((prevState) => [
+        ...prevState,
+        [i, sidoName[i], parseInt(pm25Sum / stationCount)],
+      ]);
+    });
+    console.log(average10, average25);
+  };
+
+  useEffect(() => {
+    getSidoInfo();
+  }, []);
+
   return (
     <section className={styles.rankContainer}>
-      <Container width="45%" align="center">
+      <Container align="center">
         <h1>주요 도시별 미세먼지 농도 순위</h1>
+        {average10.length === 0 &&
+          sidoName.map((element, i) => {
+            return (
+              <RankElement
+                sido={element}
+                index={i}
+                pm10={"-"}
+                pm25={"-"}
+                key={i}
+              />
+            );
+          })}
+        {average10.map((element, i) => {
+          console.log(element);
+          return (
+            <RankElement
+              sido={element[1]}
+              index={element[0]}
+              pm10={element[2]}
+              pm25={average25[i][2]}
+              key={i}
+            />
+          );
+        })}
       </Container>
     </section>
   );
