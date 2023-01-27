44c97191199a90d1ed7243f1103200140c11b914
1myeji <106291546+1myeji@users.noreply.github.com>
2023-01-27 16:22:49 +0900

    feat: Create Cart Design

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Cart.module.scss";
+import Container from "../../components/UI/Container";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const [likeIcon, setlikeIcon] = useState(true);
+
+  return (
+    <section className={styles.box}>
+      <h1>등록된 즐겨찾기</h1>
+      <Container>
+        <div className={styles.left_box}>
+          <div
+            onClick={() => {
+              setlikeIcon(!likeIcon);
+            }}
+            className={styles.like}
+          >
+            {likeIcon ? (
+              <i className="fa-solid fa-heart" style={{ color: "red" }}></i>
+            ) : (
+              <i className="fa-regular fa-heart"></i>
+            )}
+          </div>
+          <h2>충남 성성동</h2>
+        </div>
+        <div className={styles.status}>
+          <i className="fa-regular fa-face-smile"></i>
+          <p>좋음</p>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default Cart;
